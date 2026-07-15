f69c8a4da596052e9ff3d3fe97865d4283f2c411
DrTVockerodtMO <114994380+DrTVockerodtMO@users.noreply.github.com>
2026-07-15 14:32:14 +0100

    Added source code for atl_poly1d_vert_adv

--- science/adjoint/build/psyad_vars.mk
+++ science/adjoint/build/psyad_vars.mk
@@ -5,7 +5,6 @@
 ##############################################################################
 # Active variables
 all: export ACTIVE_tl_poly_advective_kernel_mod            := advective dtdx dtdy v u tracer wind
-all: export ACTIVE_tl_poly1d_vert_adv_kernel_mod           := advective wind dpdz tracer
 all: export ACTIVE_stabilise_bl_u_kernel_mod               := u_stabilised u_initial u_final
 all: export ACTIVE_apply_mixed_lu_operator_kernel_mod      := wind theta exner lhs_u lhs_t
 all: export ACTIVE_apply_mixed_operator_kernel_mod         := u_e t_col lhs_p lhs_w lhs_uv exner wind_w wind_uv
